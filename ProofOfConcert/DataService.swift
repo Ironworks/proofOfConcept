48d96bfbded6ab5f860ded56856633276d1d371a
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-03-16 10:34:19 +0000

    Initial Setup of Data Service and model

---
+++ ProofOfConcert/DataService.swift
@@ -0,0 +1,12 @@
+//
+//  DataService.swift
+//  ProofOfConcert
+//
+//  Created by Trevor Doodes on 16/03/2017.
+//  Copyright © 2017 Ironworks Media Ltd. All rights reserved.
+//
+
+import Foundation
+
+let URL = "http://somatest.ground-control.co.uk/SummerMaintService/?q=dataRequest&token=4C68F84A-CFE1- 4245-B782-300D60438CB11&parentID=&dataType=1005&dateTime=1900-01- 01T00:00:00.000&latitude=0.0&longitude=0.0&radius=0.0&rangeStart=0&rangeEnd=50"
+
---
+++ ProofOfConcert/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  ProofOfConcert
+//
+//  Created by Trevor Doodes on 16/03/2017.
+//  Copyright © 2017 Ironworks Media Ltd. All rights reserved.
+//
+
+import Foundation
+struct Visit {
+    var id: String
+    var clientName: String
+    var siteId: String
+    var clientInstructions: String
+    var startDate: String
+}
