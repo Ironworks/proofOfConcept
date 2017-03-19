46ad699eea584a13bc74ea5b26a5b5f5d411b1a5
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-03-19 22:21:29 +0000

    Implemented detail view with map for site location.

--- ProofOfConcert/DetailViewController.swift
+++ ProofOfConcert/DetailViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
+    @IBOutlet weak var clientNameLabel: UILabel!
+    @IBOutlet weak var siteIdLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem?.clientName {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        
+        if let detail = self.detailItem {
+            if let label = self.clientNameLabel {
+                label.text = detail.clientName
+                self.siteIdLabel.text = detail.siteId
+                self.mapView.centerCoordinate = (detail.location)
+                let region = MKCoordinateRegionMakeWithDistance(
+                    detail.location, 800, 800)
+                mapView.setRegion(region, animated: true)
+                let newPin = MKPointAnnotation()
+                newPin.coordinate = detail.location
+                mapView.addAnnotation(newPin)
             }
         }
+        
     }
 
     override func viewDidLoad() {
