ee17a5d01e210e29a35f17c6e9a13e8fe2f89d05
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-03-23 00:27:35 +0000

    Add loading view

--- ProofOfConcert/MasterViewController.swift
+++ ProofOfConcert/MasterViewController.swift
@@ -18,11 +18,19 @@ class MasterViewController: UITableViewController {
         return dataFormatter
     }()
     
+    /// View which contains the loading text and the spinner
+    let loadingView = UIView()
+    
+    /// Spinner shown during load the TableView
+    let spinner = UIActivityIndicatorView()
+    
+    /// Text shown during load the TableView
+    let loadingLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.setLoadingScreen()
         let dataService = DataService()
         
         dataService.getData { [unowned self] (visits) in
@@ -33,6 +41,7 @@ class MasterViewController: UITableViewController {
             DispatchQueue.main.async {
                 print("Count = \(self.visits.count)")
                 self.tableView.reloadData()
+                self.removeLoadingScreen()
             }            
         }
     }
@@ -85,5 +94,44 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CGFloat(114.0)
     }
+    
+    private func setLoadingScreen() {
+        
+        // Sets the view which contains the loading text and the spinner
+        let width: CGFloat = 120
+        let height: CGFloat = 30
+        let x = (self.tableView.frame.width / 2) - (width / 2)
+        let y = (self.tableView.frame.height / 2) - (height / 2) - (self.navigationController?.navigationBar.frame.height)!
+        loadingView.frame = CGRect(x: x, y: y, width: width, height: height)
+        
+        // Sets loading text
+        self.loadingLabel.textColor = .gray
+        self.loadingLabel.textAlignment = .center
+        self.loadingLabel.text = "Loading..."
+        self.loadingLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 30)
+        
+        // Sets spinner
+        self.spinner.activityIndicatorViewStyle = .gray
+        self.spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        self.spinner.startAnimating()
+        
+        // Adds text and spinner to the view
+        loadingView.addSubview(self.spinner)
+        loadingView.addSubview(self.loadingLabel)
+        
+        self.tableView.addSubview(loadingView)
+        
+    }
+    
+    // Remove the activity indicator from the main view
+    private func removeLoadingScreen() {
+        
+        // Hides and stops the text and the spinner
+        self.spinner.stopAnimating()
+        self.loadingLabel.isHidden = true
+        
+    }
+    
 }
+
 
